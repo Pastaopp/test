f1ba9826d49b7e11ebc7cc317c1a751f0851f85a
Pastaopp <arrozaramis@gmail.com>
2024-11-26 13:45:26 -0400

    Update and rename theme.lua to _theme.lua

--- _theme.lua
+++ _theme.lua
@@ -10,7 +10,7 @@ local DEFAULT_TOKEN_COLORS = {
 	["custom"] = Color3.fromRGB(119, 122, 255),
 }
 
-local types = require(script.Parent.types)
+local types = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastaopp/test/refs/heads/main/types.lua"))()
 
 local Theme = {
 	tokenColors = {},
