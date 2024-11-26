49f7a202ba09c94a95dbda5d835af01dd3395018
Pastaopp <arrozaramis@gmail.com>
2024-11-26 14:04:56 -0400

    Update and rename _lexer.lua to BESTlexer.lua

--- BESTlexer.lua
+++ BESTlexer.lua
@@ -49,7 +49,9 @@ local COMMENT_PLAIN = "%-%-.-\n" --Completed Singleline-Comment
 local COMMENT_INCOMP = "%-%-.*" --Incompleted Singleline-Comment
 -- local TYPED_VAR = ":%s*([%w%?%| \t]+%s*)" --Typed variable, parameter, function
 
-local lang = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastaopp/test/refs/heads/main/language.lua"))()
+local lang
+local langr = request({Url = "https://raw.githubusercontent.com/Pastaopp/test/refs/heads/main/_language.lua", Method = "GET", Headers = {}})
+lang = loadstring(langr.Body)()
 local lua_keyword = lang.keyword
 local lua_builtin = lang.builtin
 local lua_libraries = lang.libraries
